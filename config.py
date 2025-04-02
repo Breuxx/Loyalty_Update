def112e916deffaae1753612265f7fcd5264c69f
Breuxx <58144664+Breuxx@users.noreply.github.com>
2025-04-02 05:58:47 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@
 FLOWUP_LOGIN_URL = "http://flow-up.com/login"
 
 # Учётные данные для входа на Flow-Up
-FLOWUP_USERNAME = "your_flowup_username"
-FLOWUP_PASSWORD = "your_flowup_password"
+FLOWUP_USERNAME = "old_loyaltyeldservice@gmail.com"
+FLOWUP_PASSWORD = "flow2024"
 
 # Telegram Bot Token и Chat ID, куда отправлять отчёты
-TELEGRAM_BOT_TOKEN = "your_telegram_bot_token"
-TELEGRAM_CHAT_ID = "your_telegram_chat_id"  # ID чата или пользователя, куда будет отправляться отчёт
+TELEGRAM_BOT_TOKEN = "7654501983:AAGi8L3LHBck1tlu4FVvRDeUfq0FgKzCWiA"
+TELEGRAM_CHAT_ID = "-1002517831982"  # ID чата или пользователя, куда будет отправляться отчёт
 
 # Пороговые значения таймеров (в секундах)
 THRESHOLD_BREAK = 2 * 3600    # 2 часа
@@ -17,4 +17,4 @@ THRESHOLD_SHIFT = 2 * 3600    # 2 часа
 THRESHOLD_CYCLE = 10 * 3600   # 10 часов
 
 # Интервал обновления (например, каждые 60 секунд)
-UPDATE_INTERVAL = 60
+UPDATE_INTERVAL = 120
